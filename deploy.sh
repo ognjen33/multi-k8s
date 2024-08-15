44e2ef22153871cc48bb336d1839a6a851cc36d6
ognjen33 <ognjen.mavre@gmail.com>
2024-08-15 18:43:43 +0200

    Fix typo

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-docker build -t ognjen33/multi-client:latest -t ognjen33/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t ognjen33/multi-server:latest -t ognjen33/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t ognjen33/multi-worker:latest -t ognjen33/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push ognjen33/multi-client:latest
-docker push ognjen33/multi-server:latest
-docker push ognjen33/multi-worker:latest
+docker build -t ognjen3/multi-client:latest -t ognjen3/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t ognjen3/multi-server:latest -t ognjen3/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t ognjen3/multi-worker:latest -t ognjen3/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push ognjen3/multi-client:latest
+docker push ognjen3/multi-server:latest
+docker push ognjen3/multi-worker:latest
 
-docker push ognjen33/multi-client:$SHA
-docker push ognjen33/multi-server:$SHA
-docker push ognjen33/multi-worker:$SHA
+docker push ognjen3/multi-client:$SHA
+docker push ognjen3/multi-server:$SHA
+docker push ognjen3/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=ognjen33/multi-server:$SHA
-kubectl set image deployments/client-deployment client=ognjen33/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=ognjen33/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=ognjen3/multi-server:$SHA
+kubectl set image deployments/client-deployment client=ognjen3/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=ognjen3/multi-worker:$SHA
